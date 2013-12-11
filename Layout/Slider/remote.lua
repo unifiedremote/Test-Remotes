19c169ba7d555a74627bcb9775b96dc0273362d0
Philip Bergqvist <philip@unifiedintents.com>
2013-12-11 11:52:56 +0100

    Added dynamic slider test case.

--- Layout/Slider/remote.lua
+++ Layout/Slider/remote.lua
@@ -1,5 +1,19 @@
+local tid = 0;
+local pos = 0;
 
--- Metadata
+events.focus = function ()
+	tid = libs.timer.interval(function ()
+		pos = pos + 1;
+		if (pos >= 10) then
+			pos = 0;
+		end
+		layout.slider.progress = pos;
+	end, 500);
+end
+
+events.blur = function ()
+	libs.timer.cancel(tid);
+end
 
 events.action = function (name, extras)
 	print("action: " .. name);
