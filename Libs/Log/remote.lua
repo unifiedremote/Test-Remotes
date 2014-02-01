ab4ca67868f460fd0d51b88c10294594810b3e6c
Philip Bergqvist <philip@unifiedintents.com>
2014-02-01 20:36:53 +0100

    Added log library test remote.

---
+++ Libs/Log/remote.lua
@@ -0,0 +1,20 @@
+
+actions.trace = function ()
+	libs.log.trace("trace");
+end
+
+actions.info = function ()
+	libs.log.info("info");
+end
+
+actions.warn = function ()
+	libs.log.warn("warn");
+end
+
+actions.error = function ()
+	libs.log.error("error");
+end
+
+actions.print = function ()
+	print("print");
+end
