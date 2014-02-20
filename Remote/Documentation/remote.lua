91efa823644bd5640c207f6f8fafb46ac0a92280
Philip Bergqvist <philip@unifiedintents.com>
2014-02-20 15:52:41 +0100

    Added documentation test case.

---
+++ Remote/Documentation/remote.lua
@@ -0,0 +1,42 @@
+
+--@help Do something with default
+--@param value
+actions.default = function (value)
+
+end
+
+--@help Do something with string
+--@param value:string
+actions.string = function (value)
+
+end
+
+--@help Do something with number
+--@param value:number
+actions.number = function (value)
+
+end
+
+--@help Do something with key
+--@param value:key
+actions.key = function (value)
+
+end
+
+--@help Do something with keys
+--@param value:keys
+actions.keys = function (...)
+
+end
+
+--@help Do something with button
+--@param value:button
+actions.button = function (value)
+
+end
+
+--@help Do something with keys
+--@param value:buttons
+actions.buttons = function (...)
+
+end
