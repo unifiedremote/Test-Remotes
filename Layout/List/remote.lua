cea096827a67036206a147cec7f249f94b097f58
Philip Bergqvist <philip@unifiedintents.com>
2014-02-25 16:20:11 +0100

    Improved list test case.

--- Layout/List/remote.lua
+++ Layout/List/remote.lua
@@ -1,5 +1,4 @@
-
--- Metadata
+local server = libs.server;
 
 actions.tap = function (i)
 	print("tap: " .. i);
@@ -7,4 +6,15 @@ end
 
 actions.hold = function (i)
 	print("hold: " .. i);
+end
+
+actions.empty = function ()
+	server.update({ id = "list", children = {} });
+end
+
+actions.test = function ()
+	server.update({ id = "list", children = {
+		{ type = "item", text = "foo" },
+		{ type = "item", text = "bar" }
+	}});
 end
