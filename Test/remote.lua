5006905dc4e4863835e5b5ac4b7436b7663f0c59
Philip Bergqvist <philip@unifiedintents.com>
2014-10-10 17:26:23 +0200

    Added FSX

--- Test/remote.lua
+++ Test/remote.lua
@@ -1,35 +1,5 @@
 local script = require "script";
 
-actions.foo = function ()
-
-end
-
-actions.bar = function ()
-
-end
-
-actions.test = function ()
-	
-end
-
--- Events
-
-events.preload = function ()
-
-end
-
-events.create = function ()
-
-end
-
 events.focus = function ()
-
-end
-
-events.blur = function ()
-
-end
-
-events.destroy = function ()
-
+	libs.win.capture();
 end
